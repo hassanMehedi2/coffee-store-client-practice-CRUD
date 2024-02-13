fddfb588c931eafa96964e6d174a5294bca55c8f
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2024-02-14 03:14:59 +0600

    added delete function and update the items after deleting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useLoaderData } from "react-router-dom"
 import CoffeeCard from "./components/CoffeeCard";
+import { useState } from "react";
 
 
 function App() {
   const coffees = useLoaderData();
+  const [updatedCoffees,setUpdatedCoffees]= useState(coffees);
 
   return (
     <>
@@ -11,7 +13,7 @@ function App() {
       <h1 className='text-5xl text-purple-600 text-center my-12'>Hot Hot Cool Coffee !! {coffees.length}</h1>
       <div className="mx-32 grid md:grid-cols-2 gap-8">
       {
-          coffees.map(coffee=> <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+          updatedCoffees.map(coffee=> <CoffeeCard key={coffee._id} updatedCoffees={updatedCoffees} setUpdatedCoffees={setUpdatedCoffees} coffee={coffee}></CoffeeCard>)
       }
       </div>
       
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
-const CoffeeCard = ({ coffee }) => {
-    const { coffeeName, quantity, supplier, taste, category, details, photo } = coffee;
+const CoffeeCard = ({ coffee ,setUpdatedCoffees,updatedCoffees }) => {
+    const { _id, coffeeName, supplier, taste, details, photo } = coffee;
+
+    const handleDelete = _id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                // deleting req for id ... 
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            //Updating the new existing coffee
+                            const remaining = updatedCoffees?.filter(coffee => coffee._id !== _id);
+                            setUpdatedCoffees(remaining)
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div>
             <div className="card card-side bg-base-100 shadow-xl flex p-5">
@@ -13,10 +48,11 @@ const CoffeeCard = ({ coffee }) => {
                     <p>{details}</p>
                 </div>
                 <div className='flex flex-col gap-3'>
-                        <button className='btn bg-green-400 '>View</button>
-                        <button className='btn bg-teal-300 '>Update</button>
-                        <button className='btn bg-red-600 text-white'>Delete</button>
-                    </div>
+                    <button className='btn bg-green-400 '>View</button>
+                    <button className='btn bg-teal-300 '>Update</button>
+
+                    <button onClick={() => handleDelete(_id)} className='btn bg-red-600 text-white'>Delete</button>
+                </div>
             </div>
         </div>
     );
